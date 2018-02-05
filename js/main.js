e1b062eeeeebd379a56703882d532178be0ef2fa
benBen <benmbenjamin@gmail.com>
2018-02-05 11:37:58 -0800

    blah

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@
 
 	var loc = window.location.href
 	var baseUrl = loc.match(':5000') /* lol */ ?
-								'http://bb.ngrok.io' : '/api'
+								'http://bb.ngrok.io' : 'api'
 
 	var doc = document.documentElement;
 	var main = document.querySelector('main');
